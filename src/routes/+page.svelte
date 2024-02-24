a2594fac1afb94be03791c5967c15f5f714588ce
Ross <ross.h.sanderson@gmail.com>
2024-02-24 16:16:55 +1100

    updated message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
-<h1>Hi Anita 👋</h1>
-<p>
-	I promise to make you a nice portfolio site and so sorry to hear about the AppleID hacking
-	incident.
-</p>
+<div class="p-16 justify-center text-center">
+	<h1 class="font-black">Hi Anita 👋</h1>
+	<p class="pt-6">
+		I promise to make you a nice portfolio site and so sorry to hear about the AppleID hacking
+		incident.
+	</p>
+	<p class="italic pt-6">Ross ❤️</p>
+</div>
